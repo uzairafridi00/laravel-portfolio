774c7f1a62995520aa00a0e7fdf1db64f22a690b
Uzair Afridi <uzairafridi00@gmail.com>
2022-05-06 11:13:12 +0500

    Layouts and Files has been set
    All the layouts and master blade files have been created and adjusted

---
+++ resources/views/layouts/libraries.blade.php
@@ -0,0 +1,34 @@
+ <!-- Favicons -->
+ <link href="{{ url('assets/img/favicon.png') }}" rel="icon">
+ <link href="{{ url('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+ <!-- Google Fonts -->
+ <link
+     href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+     rel="stylesheet">
+
+ <!-- Vendor CSS Files -->
+ <link href="{{ url('assets/vendor/aos/aos.css') }}" rel="stylesheet">
+ <link href="{{ url('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+ <link href="{{ url('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+ <link href="{{ url('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+ <link href="{{ url('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+ <link href="{{ url('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+
+ <!-- Template Main CSS File -->
+ <link href="{{ url('assets/css/style.css') }}" rel="stylesheet">
+
+
+ <!-- Vendor JS Files -->
+ <script src="{{ url('assets/vendor/purecounter/purecounter.js') }}"></script>
+ <script src="{{ url('assets/vendor/aos/aos.js') }}"></script>
+ <script src="{{ url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+ <script src="{{ url('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+ <script src="{{ url('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+ <script src="{{ url('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+ <script src="{{ url('assets/vendor/typed.js/typed.min.js') }}"></script>
+ <script src="{{ url('assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
+ <script src="{{ url('assets/vendor/php-email-form/validate.js') }}"></script>
+
+ <!-- Template Main JS File -->
+ <script src="{{ url('assets/js/main.js') }}"></script>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('frontend.index');
+});
+
+Route::get('/about', function () {
+    return view('frontend.about');
+});
+
+Route::get('/resume', function () {
+    return view('frontend.resume');
+});
+
+Route::get('/services', function () {
+    return view('frontend.services');
+});
+
+Route::get('/portfolio', function () {
+    return view('frontend.portfolio');
+});
+
+Route::get('/contact', function () {
+    return view('frontend.contact');
 });
