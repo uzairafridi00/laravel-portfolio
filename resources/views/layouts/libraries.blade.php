92af9827c0f75308dda16c9717e7c8d8ec3fbd3d
Uzair Afridi <uzairafridi00@gmail.com>
2022-05-10 21:27:58 +0500

    Secure_asset has been set because of Server

--- resources/views/layouts/libraries.blade.php
+++ resources/views/layouts/libraries.blade.php
@@ -1,34 +1,34 @@
  <!-- Favicons -->
- <link href="{{ url('assets/img/favicon.png') }}" rel="icon">
- <link href="{{ url('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+ <link href="{{ secure_asset('assets/img/favicon.png') }}" rel="icon">
+ <link href="{{ secure_asset('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
  <!-- Google Fonts -->
- <link
+ {{-- <link
      href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-     rel="stylesheet">
+     rel="stylesheet"> --}}
 
  <!-- Vendor CSS Files -->
- <link href="{{ url('assets/vendor/aos/aos.css') }}" rel="stylesheet">
- <link href="{{ url('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
- <link href="{{ url('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
- <link href="{{ url('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
- <link href="{{ url('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
- <link href="{{ url('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+ <link href="{{ secure_asset('assets/vendor/aos/aos.css') }}" rel="stylesheet">
+ <link href="{{ secure_asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+ <link href="{{ secure_asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+ <link href="{{ secure_asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+ <link href="{{ secure_asset('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+ <link href="{{ secure_asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
  <!-- Template Main CSS File -->
- <link href="{{ url('assets/css/style.css') }}" rel="stylesheet">
+ <link href="{{ secure_asset('assets/css/style.css') }}" rel="stylesheet">
 
 
  <!-- Vendor JS Files -->
- <script src="{{ url('assets/vendor/purecounter/purecounter.js') }}"></script>
- <script src="{{ url('assets/vendor/aos/aos.js') }}"></script>
- <script src="{{ url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
- <script src="{{ url('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
- <script src="{{ url('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
- <script src="{{ url('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
- <script src="{{ url('assets/vendor/typed.js/typed.min.js') }}"></script>
- <script src="{{ url('assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
- <script src="{{ url('assets/vendor/php-email-form/validate.js') }}"></script>
+ <script src="{{ secure_asset('assets/vendor/purecounter/purecounter.js') }}"></script>
+ <script src="{{ secure_asset('assets/vendor/aos/aos.js') }}"></script>
+ <script src="{{ secure_asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+ <script src="{{ secure_asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+ <script src="{{ secure_asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+ <script src="{{ secure_asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+ <script src="{{ secure_asset('assets/vendor/typed.js/typed.min.js') }}"></script>
+ <script src="{{ secure_asset('assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
+ <script src="{{ secure_asset('assets/vendor/php-email-form/validate.js') }}"></script>
 
  <!-- Template Main JS File -->
- <script src="{{ url('assets/js/main.js') }}"></script>
+ <script src="{{ secure_asset('assets/js/main.js') }}"></script>
